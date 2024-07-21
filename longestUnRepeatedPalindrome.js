628718a9d28bc755971d33053ed3480f0026b819
gowtham kishore vijay <gowthamkishore3@gmail.com>
2024-07-21 13:26:15 -0700

    Adding code for unrepeated character

---
+++ longestUnRepeatedPalindrome.js
@@ -0,0 +1,23 @@
+var lengthOfLongestSubstring = function(s) {
+
+
+    if(s.length == 1){
+        return 1;
+    }
+    const hashMap = new Map();
+    let left = 0;
+    let max = 0;
+
+    for(let i =0; i< s.length; i++){
+        if(hashMap.has(s[i]) && hashMap.get(s[i]) >= left) {
+            left = hashMap.get(s[i])+1;
+        }
+        hashMap.set(s[i], i);
+        max = Math.max(max, (i-left)+1)
+    }
+    return max;
+    
+};
+
+
+console.log(lengthOfLongestSubstring("abba"))
