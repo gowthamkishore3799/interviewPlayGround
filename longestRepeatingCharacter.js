9235bef40c13b52b00449cb52959e17aec12377b
gowtham kishore vijay <gowthamkishore3@gmail.com>
2024-07-21 12:31:50 -0700

    Adding code for longestRepeatingCharacter.js

---
+++ longestRepeatingCharacter.js
@@ -0,0 +1,34 @@
+var characterReplacement = function(s, k) {
+    let leftPointer = 0;
+    let max = 0;
+    let hashMap = {}
+    let maxFreq = 0
+
+
+    for(let rightPointer = 0; rightPointer < s.length; rightPointer++){
+        hashMap[s[rightPointer]] = (hashMap[s[rightPointer]] || 0) +1;
+
+
+        maxFreq = Math.max(maxFreq, hashMap[s[rightPointer]])
+  
+        // Move the window from the left until reaching `k` replacements
+        if (maxFreq + k < rightPointer - leftPointer + 1) {
+          hashMap[s[leftPointer]]--
+          leftPointer++
+        }
+  
+
+
+
+        max = Math.max(max, rightPointer-leftPointer+1);
+    }
+
+    return max
+};
+
+
+console.log(characterReplacement("AAABBBA", 1))
+
+
+
+// 2+1 < 
