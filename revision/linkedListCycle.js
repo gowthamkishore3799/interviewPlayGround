a129b7557e8159eae40d58bf356c61a50b9cc6ac
Gowtham Kishore <gowthamkishore3@gmail.com>
2025-02-02 12:15:00 -0800

    ADding code for hascycle

---
+++ revision/linkedListCycle.js
@@ -0,0 +1,13 @@
+var hasCycle = function(head) {
+    // using fast pointer 
+
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next){
+        slow = slow.next
+        fast = fast.next.next;
+        if (slow === fast) return true
+    }
+    return false;
+};
