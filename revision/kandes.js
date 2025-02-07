0dbc69b275fffed2a2399e4cf6481becc47adf85
Gowtham Kishore <gowthamkishore3@gmail.com>
2025-02-06 23:05:05 -0800

    Adding code

---
+++ revision/kandes.js
@@ -0,0 +1,16 @@
+function maxSubarraySum(arr) {
+    let res = arr[0];
+    let maxEnding = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        
+        // Find the maximum sum ending at index i by either extending 
+        // the maximum sum subarray ending at index i - 1 or by
+        // starting a new subarray from index i
+        maxEnding = Math.max(maxEnding + arr[i], arr[i]);
+        
+        // Update res if maximum subarray sum ending at index i > res
+        res = Math.max(res, maxEnding);
+    }
+    return res;
+}
