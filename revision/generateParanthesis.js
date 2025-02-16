0eb1156ba8cfc8fda5fcecb73236d3720ccc52ef
Gowtham Kishore <gowthamkishore3@gmail.com>
2025-02-15 21:31:11 -0800

    Adding code for paranthesis

---
+++ revision/generateParanthesis.js
@@ -0,0 +1,25 @@
+var generateParenthesis = function(n) {
+    const res = []
+    const go = (left, right, index, s) =>{
+
+        if (s.length === 2 * index) {
+            res.push(s);
+            return;
+          }
+
+
+    if(left<index) {
+        go(left+1, right, index, s+ "(")
+    } 
+
+    if(right< left){
+        go(left, right+1, index, s+")")
+    }
+
+}
+
+go(0,0, n, "")
+
+return res
+
+};
